d0c292136d350f15497c19a20292c286588be355
thanzou <axelromain74@gmail.com>
2024-09-25 11:29:28 +0100

    erreur

--- app/components/a_propos/A_propos.jsx
+++ app/components/a_propos/A_propos.jsx
@@ -1,1 +1,19 @@
 
+export function A_propos () {
+    return (
+       <>
+       <section className="w-full text-center">
+        <h1>histoire</h1>
+        <div className="grid grid-cols-1 gap-3 p-3">
+            <img className="w-full h-auto" src="../assets/out1.jpg" alt="" />
+             <div className="flex flex-col gap-3">
+                <p className="text-justify">
+                Fondé en 2010 par la passionnée de cuisine Marie Okoumé <br />, Saveurs Gabonaises est né du désir de partager <br /> l'authentique cuisine gabonaise avec le monde. Notre <br /> voyage culinaire a commencé dans une petite cuisine <br /> familiale et s'est transformé en l'expérience <br /> gastronomique que nous offrons aujourd'hui.
+                </p>
+                <p className="text-justify">Situé au cœur de Libreville, notre restaurant est devenu <br /> un lieu de rencontre pour les amateurs de cuisine <br /> gabonaise authentique et les curieux culinaires du monde <br /> entier. Nous nous efforçons de créer non seulement des <br /> repas, mais des expériences qui racontent l'histoire riche <br /> et diverse du Gabon à travers ses saveurs uniques.</p>
+             </div>
+        </div>
+       </section>
+       </> 
+    )
+}
